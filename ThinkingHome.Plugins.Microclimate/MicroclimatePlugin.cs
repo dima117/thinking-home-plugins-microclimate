7f16f81090d70a6b60d0a6d664fe74abfa1922fa
dima117 <dima117a@gmail.com>
2014-10-12 19:25:40 +0400

    - db mapping;
    - simple http api;

--- ThinkingHome.Plugins.Microclimate/MicroclimatePlugin.cs
+++ ThinkingHome.Plugins.Microclimate/MicroclimatePlugin.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NHibernate.Hql.Ast.ANTLR;
 using NHibernate.Linq;
+using NHibernate.Mapping.ByCode;
 using ThinkingHome.Core.Plugins;
+using ThinkingHome.Plugins.Listener.Api;
+using ThinkingHome.Plugins.Listener.Attributes;
 using ThinkingHome.Plugins.Microclimate.Model;
 using ThinkingHome.Plugins.NooLite;
 
@@ -14,10 +13,18 @@ namespace ThinkingHome.Plugins.Microclimate
 	[Plugin]
 	public class MicroclimatePlugin : PluginBase
     {
+		public override void InitDbModel(ModelMapper mapper)
+		{
+			mapper.Class<TemperatureSensor>(cfg => cfg.Table("Microclimate_TemperatureSensor"));
+			mapper.Class<TemperatureData>(cfg => cfg.Table("Microclimate_TemperatureData"));
+		}
+
 		[OnMicroclimateDataReceived]
 		public void MicroclimateDataReceived(int channel, decimal temperature, int humidity)
 		{
 			var now = DateTime.Now;
+
+			Logger.Debug("microclimate data received: c={0}, t={1}, h={2}", channel, temperature, humidity);
 
 			using (var session = Context.OpenSession())
 			{
@@ -26,10 +33,13 @@ namespace ThinkingHome.Plugins.Microclimate
 					.Where(s => s.Channel == channel)
 					.ToList();
 
+				Logger.Debug("{0} sensors was found", sensors.Count);
+
 				foreach (var sensor in sensors)
 				{
 					var data = new TemperatureData
 					{
+						Id = Guid.NewGuid(),
 						CurrentDate = now,
 						Temperature = Convert.ToInt32(temperature),
 						Humidity = humidity,
@@ -42,5 +52,62 @@ namespace ThinkingHome.Plugins.Microclimate
 				session.Flush();
 			}
 		}
+
+		#region api
+
+		[HttpCommand("/api/microclimate/sensors/add")]
+		public object AddSensor(HttpRequestParams request)
+		{
+			var channel = request.GetRequiredInt32("channel");
+			var displayName = request.GetRequiredString("displayName");
+			var showHumidity = request.GetRequiredBool("showHumidity");
+
+			Logger.Debug("add sensor: channel={0}; displayName={1}; showHumidity={2}", channel, displayName, showHumidity);
+
+			using (var session = Context.OpenSession())
+			{
+				var sensor = new TemperatureSensor
+				{
+					Id = Guid.NewGuid(),
+					Channel = channel,
+					DisplayName = displayName,
+					ShowHumidity = showHumidity
+				};
+
+				session.Save(sensor);
+				session.Flush();
+
+				return sensor.Id;
+			}
+		}
+
+		[HttpCommand("/api/microclimate/data")]
+		public object GetSensorData(HttpRequestParams request)
+		{
+			var sensorId = request.GetRequiredGuid("sensorId");
+
+			using (var session = Context.OpenSession())
+			{
+				var data = session.Query<TemperatureData>()
+					.Where(d => d.Sensor.Id == sensorId)
+					.OrderByDescending(d => d.CurrentDate)
+					.Take(20)
+					.ToList();
+
+				return data.Select(CreateModel).ToList();
+			}
+		}
+
+		private object CreateModel(TemperatureData arg)
+		{
+			return new
+			{
+				d = arg.CurrentDate,
+				t = arg.Temperature,
+				h = arg.Humidity
+			};
+		}
+
+		#endregion
     }
 }
