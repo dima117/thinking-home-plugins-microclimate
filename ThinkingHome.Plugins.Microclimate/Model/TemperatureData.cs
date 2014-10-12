cbb180bf627ef6d7c07ca4eef43e7bbebf510bd7
dima117 <dima117a@gmail.com>
2014-10-12 16:56:20 +0400

    - oops, we forgot a column for date;

--- ThinkingHome.Plugins.Microclimate/Model/TemperatureData.cs
+++ ThinkingHome.Plugins.Microclimate/Model/TemperatureData.cs
@@ -7,6 +7,8 @@ namespace ThinkingHome.Plugins.Microclimate.Model
 	{
 		public virtual Guid Id { get; set; }
 
+		public virtual DateTime CurrentDate { get; set; }
+
 		public virtual TemperatureSensor Sensor { get; set; }
 
 		public virtual int Temperature { get; set; }
