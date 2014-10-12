6799b7e2f0fb7fc30d0d3d2a4b5cec45cb97fa10
dima117 <dima117a@gmail.com>
2014-10-12 16:13:25 +0400

    - blank microclimate plugin;

---
+++ ThinkingHome.Plugins.Microclimate/MicroclimatePlugin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThinkingHome.Core.Plugins;
+
+namespace ThinkingHome.Plugins.Microclimate
+{
+	[Plugin]
+	public class MicroclimatePlugin : PluginBase
+    {
+		public override void InitPlugin()
+		{
+			Logger.Debug("init");
+			base.InitPlugin();
+		}
+
+		public override void StartPlugin()
+		{
+			Logger.Debug("start");
+			base.StartPlugin();
+		}
+
+		public override void StopPlugin()
+		{
+			Logger.Debug("stop");
+			base.StopPlugin();
+		}
+    }
+}
