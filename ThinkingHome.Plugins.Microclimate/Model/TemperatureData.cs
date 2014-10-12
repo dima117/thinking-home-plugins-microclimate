d6947382b8c84f3be66d284c8b52ee5372849073
dima117 <dima117a@gmail.com>
2014-10-12 16:43:43 +0400

    - data model;

---
+++ ThinkingHome.Plugins.Microclimate/Model/TemperatureData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ThinkingHome.Plugins.Microclimate.Model
+{
+	
+	public class TemperatureData
+	{
+		public virtual Guid Id { get; set; }
+
+		public virtual TemperatureSensor Sensor { get; set; }
+
+		public virtual int Temperature { get; set; }
+
+		public virtual int Humidity { get; set; }
+	}
+}
--- ThinkingHome.Plugins.Microclimate/Properties/AssemblyInfo.cs
+++ ThinkingHome.Plugins.Microclimate/Properties/AssemblyInfo.cs
@@ -5,6 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
+using ECM7.Migrator.Framework;
+
+[assembly: MigrationAssembly("ThinkingHome.Plugins.Microclimate")]
+
+
 [assembly: AssemblyTitle("ThinkingHome.Plugins.Microclimate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
